b57565cdbbec10e920f45a082ef4dcd23be70718
Sushanta Das <sushanta@easyrewardz.com>
2021-01-05 12:44:26 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
-
+import About from './About';
+import Home from './Home';
+import Management from './Management'
+import Contact from "./Contact";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Router>
+          <div>
+
+            {/* <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/Contact">Contact</Link>
+                </li>
+              </ul>
+            </nav> */}
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Navbar bg="dark" variant="dark">
+              <Nav className="mr-auto">
+                <Nav.Link href="#Home"><Link to="/">Home</Link></Nav.Link>
+                <Nav.Link href="#About"><Link to="/About">About</Link></Nav.Link>
+                <Nav.Link href="#Contact"><Link to="/Contact">Contact</Link></Nav.Link>
+                <Nav.Link href="#Management"><Link to="/Management">Management</Link></Nav.Link>
+              </Nav>
+
+            </Navbar>
+            <Switch>
+           
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/Contact">
+                <Contact />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+              <Route path="/Management">
+                <Management />
+              </Route>
+              
+            </Switch>
+          </div>
+
+
+        </Router>
+        <br />
+
+        <br />
+
+      </>
+      {/* {<About/>}
+      {<Home/>}
+      {<Contact/>} */}
     </div>
   );
 }
---
+++ src/Management.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function Management(){
+return(
+    <div>Management page</div>
+)
+}
+export default Management;
